bc863168d36c53d2b606b78340eed996cd6c8da7
Mindaugas Kapickas <mindaugas.kapickas@visma.com>
2021-08-03 14:31:46 +0300

    working on carousel interac, sticky nav css done

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const arrowLeft = document.querySelector('.carousel__button--left')
-const arrowRight = document.querySelector('.carousel__button--left')
+// CAROUSEL
+const slideTrack = document.querySelector('.carousel__track')
 
-arrowLeft.addEventListener("click", function(){
-    console.log('clicked left arrow');
-})
+// all slides
+const slides = Array.from(slideTrack.children)
 
-arrowLeft.addEventListener("click", function(){
-    console.log('clicked right arrow');
-})
+console.log(slideTrack);
+console.log(slides);
+
+// ARROWS
+const arrowLeft = document.querySelector('.carousel__button--left');
+const arrowRight = document.querySelector('.carousel__button--right');
+
+// SLIDES
+const firstSlide = document.querySelector('.first')
+const secondSlide = document.querySelector('.second')
+const thirdSlide = document.querySelector('.third')
+
+arrowLeft.addEventListener("click", ()=>{
+    console.log('clicked');
+});
+
+arrowRight.addEventListener("click", ()=>{
+    
+    firstSlide.className.add = 'display-none';
+    secondSlide.classList.remove = 'display-none'
+});
