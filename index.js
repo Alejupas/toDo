d2b84d2706aafb6bf0a25e8714689605235b2436
Mindaugas Kapickas <mindaugas.kapickas@visma.com>
2021-08-03 19:38:53 +0300

    image added in carousel. working on arrows, title

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 const counter = 0;
-const track = document.querySelector('.carousel-container')
 
-const allSlidesArr = ["https://placeimg.com/1280/720/nature", "https://placeimg.com/1280/720/animals", "https://placeimg.com/1280/720/tech"]
+const carousel = document.getElementById('carousel')
+const imageContainer = document.querySelector('.carousel')
+
+const imageArr = ["https://placeimg.com/1280/720/nature", "https://placeimg.com/1280/720/animals", "https://placeimg.com/1280/720/tech"]
+const heroPhrase = ['Good things', 'Welcome!', 'Wonderful day']
 
 // ARROWS
 const arrowLeft = document.querySelector('.carousel__button--left');
 const arrowRight = document.querySelector('.carousel__button--right');
 
+// FUNCTIONS
+const firstImg = () => {
+    counter < 0;
+}
+
+const lastImg = () => {
+    counter > imageArr.length - 1;
+}
+
 arrowLeft.addEventListener("click", ()=>{
-    
+   const flipLeft = () => {
+    counter--;
+    if(firstImg()) counter = imageArr.length - 1;
+    return
+    }
 });
 
 arrowRight.addEventListener("click", ()=>{
-    
+const filpRight = () => {
+    counter++;
+    if(lastImg()) counter = 0;
+    return
+    }
 });
 
+const addHtml = () => {
+    carousel.style.backgroundImage = `url(${imageArr[counter]})`;
+    imageContainer.innerHTML = `<h2>is good.</h2>`;
+    imageContainer.style.fontSize = '25px';
+    imageContainer.style.zIndex = '12';
 
-const getFirstImage = () => {
-        track.style.backgroundImage = `url(${this.items[this.counter].image})`;
-		imageContainer.innerHTML = `<h2>${this.items[this.counter].name}</h2>`;
+    return;
 }
+addHtml()
