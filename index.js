a3bf4f7a6f78e8abfb511095639dd45f67161980
Mindaugas Kapickas <mindaugas.kapickas@visma.com>
2021-08-03 13:08:06 +0300

    listeners for arrows check

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('js here')
+const arrowLeft = document.querySelector('.carousel__button--left')
+const arrowRight = document.querySelector('.carousel__button--left')
+
+arrowLeft.addEventListener("click", function(){
+    console.log('clicked left arrow');
+})
+
+arrowLeft.addEventListener("click", function(){
+    console.log('clicked right arrow');
+})
