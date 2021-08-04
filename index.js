3e0ec51d2d97aa3f63693194318369eb936d22e4
Mindaugas Kapickas <mindaugas.kapickas@visma.com>
2021-08-04 09:28:07 +0300

    interactivity carousel works.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const counter = 0;
+let counter = 0;
 
 const carousel = document.getElementById('carousel')
 const imageContainer = document.querySelector('.carousel')
 
-const imageArr = ["https://placeimg.com/1280/720/nature", "https://placeimg.com/1280/720/animals", "https://placeimg.com/1280/720/tech"]
+const imageArr = ['https://placeimg.com/1280/720/nature', 'https://placeimg.com/1280/720/animals', 'https://placeimg.com/1280/720/tech']
 const heroPhrase = ['Good things', 'Welcome!', 'Wonderful day']
 
 // ARROWS
@@ -12,34 +12,42 @@ const arrowRight = document.querySelector('.carousel__button--right');
 
 // FUNCTIONS
 const firstImg = () => {
-    counter < 0;
+   return counter < 0;
 }
 
 const lastImg = () => {
-    counter > imageArr.length - 1;
+   return counter > imageArr.length - 1;
 }
 
-arrowLeft.addEventListener("click", ()=>{
-   const flipLeft = () => {
+const flipLeft = () => {
     counter--;
-    if(firstImg()) counter = imageArr.length - 1;
+    if(firstImg())  counter = imageArr.length - 1;
     return
-    }
-});
+}
 
-arrowRight.addEventListener("click", ()=>{
+
 const filpRight = () => {
     counter++;
     if(lastImg()) counter = 0;
     return
-    }
+}
+
+arrowLeft.addEventListener('click', ()=>{
+  flipLeft()
+  addHtml()
+});
+
+arrowRight.addEventListener('click', ()=>{
+  filpRight()
+  addHtml()
 });
 
 const addHtml = () => {
     carousel.style.backgroundImage = `url(${imageArr[counter]})`;
-    imageContainer.innerHTML = `<h2>is good.</h2>`;
+    imageContainer.innerHTML = `<h2>${heroPhrase[counter]}</h2>`;
     imageContainer.style.fontSize = '25px';
-    imageContainer.style.zIndex = '12';
+    imageContainer.style.color = 'white';
+    imageContainer.style.textAlign = 'center';
 
     return;
 }
