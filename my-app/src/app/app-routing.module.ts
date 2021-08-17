8d5a4745c4abeeed0ae83c02cc770601ea683712
Mindaugas Kapickas <mindaugas.kapickas@visma.com>
2021-08-17 16:46:33 +0300

    edit component + naigation sticky and box shadow

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
-import { CarouselComponent } from './components/rendering/carousel/carousel.component';
 import { CreateComponent } from './components/rendering/create/create.component'
 import { AllitemsComponent } from './components/allitems/allitems.component';
 import { EditComponent } from './components/edit/edit.component';
@@ -9,7 +8,6 @@ import { EditComponent } from './components/edit/edit.component';
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'create', component: CreateComponent},
-  {path: 'home', component: CarouselComponent },
   {path: 'edit', component: EditComponent },
   {path: 'allItems', component: AllitemsComponent }
 ];
